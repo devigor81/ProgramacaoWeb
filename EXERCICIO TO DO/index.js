1898012f828591706afb9d95c7836811b954ddaf
Igor Santos <igor.00000847958@unicap.br>
2023-05-12 22:59:59 -0300

    :sparkles: feat: Concluído CRUD das atividades consumindo da API

---
+++ EXERCICIO TO DO/index.js
@@ -0,0 +1,172 @@
+const lista = document.getElementById("lista");
+const btBuscar = document.getElementById("btBuscar");
+
+const inputCheck = document.getElementById("inputCheck");
+const inputDescricao = document.getElementById("inputDescricao");
+const btAdicionar = document.getElementById("btAdicionar");
+
+let rota = "https://parseapi.back4app.com/classes/Task";
+
+const adicionarLista = (atividades) => {
+
+    lista.innerHTML = "";
+
+    for (atividade of atividades) {
+
+        const novaLinha = document.createElement("tr");
+
+        const campoDescricao = document.createElement("td");
+        const campoCheck = document.createElement("td");
+        const inputCheck = document.createElement("input");
+        const btDeletar = document.createElement("button")
+
+        inputCheck.type = "checkbox";
+        inputCheck.checked = (atividade.done) ? true : false;
+
+        campoDescricao.innerHTML = atividade.description;
+        btDeletar.innerHTML = "X";
+
+        campoCheck.appendChild(inputCheck);
+
+        novaLinha.appendChild(campoCheck);
+        novaLinha.appendChild(campoDescricao);
+        novaLinha.appendChild(btDeletar);
+
+        novaLinha.id = atividade.objectId;
+
+        lista.appendChild(novaLinha);
+
+        lista.addEventListener("change", (event) => {
+            if (event.target.type === "checkbox") {
+                const id = event.target.parentNode.parentNode.id;
+
+                for (atividade of atividades) {
+                    if (atividade.objectId === id) {
+                        atualizarAtividade(id, atividade)
+                    }
+                }
+            }
+        })
+
+    }
+
+}
+
+
+lista.addEventListener("click", (event) => {
+    if (event.target.type === "submit") {
+        const id = event.target.parentNode.id;
+        console.log(event.target.parentNode)
+        deletarAtividade(id);
+
+        console.log(event.target.type);
+    }
+})
+
+
+const obterListaAtividades = () => {
+    fetch(rota, {
+        headers: {
+            "X-Parse-Application-Id": "yTRg1s611eOU1JRaZkA6GWQgJvX6VYpSyEYZ9fu8",
+            "X-Parse-REST-API-Key": "lqNrSsVMu7HdulOKn013hK0IvZ1hgUFJpwYHpaLo"
+        }
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            adicionarLista(data.results);
+        })
+}
+
+obterListaAtividades();
+
+const atualizarAtividade = (id, atividade) => {
+
+    let check = (atividade.done) ? false : true;
+
+    const params = {
+        method: 'PUT',
+        headers: {
+            "X-Parse-Application-Id": "yTRg1s611eOU1JRaZkA6GWQgJvX6VYpSyEYZ9fu8",
+            "X-Parse-REST-API-Key": "lqNrSsVMu7HdulOKn013hK0IvZ1hgUFJpwYHpaLo",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ done: check })
+    };
+
+    fetch(rota + `/${id}`, params)
+        .catch((error) => {
+            console.log(error);
+        })
+
+    obterListaAtividades;
+}
+
+const deletarAtividade = (id) => {
+    const params = {
+        method: 'DELETE',
+        headers: {
+            "X-Parse-Application-Id": "yTRg1s611eOU1JRaZkA6GWQgJvX6VYpSyEYZ9fu8",
+            "X-Parse-REST-API-Key": "lqNrSsVMu7HdulOKn013hK0IvZ1hgUFJpwYHpaLo",
+        }
+    };
+
+    fetch(rota + `/${id}`, params)
+        .then(() => {
+            obterListaAtividades();
+        })
+        .catch((error) => {
+            console.log(error);
+    })
+
+}
+
+const novaAtividade = () => {
+
+    if(!inputDescricao.value) {
+        alert("É necessário digitar a descrição da atividade");
+        return;
+    }
+
+    const params = {
+        method: 'POST',
+        headers: {
+            "X-Parse-Application-Id": "yTRg1s611eOU1JRaZkA6GWQgJvX6VYpSyEYZ9fu8",
+            "X-Parse-REST-API-Key": "lqNrSsVMu7HdulOKn013hK0IvZ1hgUFJpwYHpaLo",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            done: inputCheck.checked,
+            description: inputDescricao.value
+        })
+    };
+    btAdicionar.disabled = true;
+    fetch(rota, params)
+        .then((res) => res.JSON)
+        .then(() => {
+            btAdicionar.disabled = false;
+            inputCheck.checked = (inputCheck.checked) ? false : false;
+            inputDescricao.value = "";
+            obterListaAtividades();
+            btAdicionar.disabled = false;
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+
+}
+
+
+btBuscar.onclick = obterListaAtividades;
+btAdicionar.onclick = novaAtividade;
+
+
+
+
+
+
+
+
+
+
+
+
